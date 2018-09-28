b0dd50c9e29eb927937dcb0b5ebc8d4b120652c6
Aengelus <getzinger.sascha@gmail.com>
2018-09-28 22:22:20 +0200

    Implementation from get request and Data displaying

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http, Response, RequestOptions, Headers, URLSearchParams} from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Frontend Test Application';
+
+  // If you like to access a variable in your html, assign it first here.
+  response: String;
+
+  apiRoot: String = 'http://httpbin.org';
+
+  constructor(private http: Http) { }
+
+  doGET(search_value) {
+    console.log("GET");
+    let url = `${this.apiRoot}/get`;
+    let search = new URLSearchParams();
+    search.set('username', search_value);
+    search.set('limit', '25');
+    this.http.get(url, {search}).subscribe(
+        res => {
+          this.response = JSON.parse(res._body);
+          console.log(this.response);
+        },
+        error => console.log(error)
+      );
+  }
+
 }
